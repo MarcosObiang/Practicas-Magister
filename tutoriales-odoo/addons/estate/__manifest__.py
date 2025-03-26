45deff8f2966349ea729617f2667f2f6f4af76d7
Marcos Obiang <marcosobiang98@gmail.com>
2025-03-26 15:36:14 +0100

    añadiendo barra de busqeda

--- tutoriales-odoo/addons/estate/__manifest__.py
+++ tutoriales-odoo/addons/estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate Module",
-    "version": "0.1",
+    "version": "0.2",
     "author": "yo",
     "summary": "Módulo para la gestión de bienes raíces",
     "description": """
@@ -10,4 +10,13 @@
     "installable": True,
     "application": True,
     "auto_install": False,
+    "data":[
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+        "views/estate_fields_list.xml",
+        "views/form_fields.xml",
+        "views/search_field.xml"
+
+    ]
 }
--- tutoriales-odoo/addons/estate/models/estate.py
+++ tutoriales-odoo/addons/estate/models/estate.py
@@ -1,21 +1,22 @@
 
 from odoo import models,fields
 
-class Estate(models.Model):
+class TestModel(models.Model):
     _name ="test_model"
-    _description="Casas"
+    _description="Test Model"
 
-    name=fields.Char("Model name", required=True,translate=True)
+    name=fields.Char("Name", required=True,translate=True)
     description=fields.Char("Description", required =True,translate=True)
     postcode=fields.Char("Post code", required=True)
-    date_availability=fields.Date("Availability date")
+    date_availability=fields.Date("Availability date",copy=False,default=lambda self: fields.Date.add(fields.Date.today(),months=3))
     expected_price=fields.Float('Expected price', required =True)
-    selling_price=fields.Float('selling price')
-    bedrooms=fields.Integer('Bedrooms')
+    selling_price=fields.Float('selling price', readonly=True,copy=False)
+    bedrooms=fields.Integer('Bedrooms',default=2)
     living_area=fields.Integer('Living area')
     facades=fields.Integer('Facades')
     garage= fields.Boolean('Has Garage')
     garden= fields.Boolean('Has Garden')
     garden_area=fields.Integer('Total garden area')
-    garden_orienation= fields.Selection(string='Type',selection=[('north','North'),('south','South'),('east','East'),('north','West')])
-
+    garden_orientation= fields.Selection(string='Orientation',selection=[('north','North'),('south','South'),('east','East'),('north','West')])
+    active=fields.Boolean("Active",default=False,translate=True)
+    state= fields.Selection(string = 'Estado', selection=[("new","New"),("offer_recieved","Offer Recieved"),("offer_accepted","Offer Acepted"),("sold","Sold"),("canceled","Canceled")])
